367cc54022280a31c2f08ee10c225fe3df2ebd9d
FUMEI128 <qwer12356q>
2020-09-30 16:46:06 +0800

    commit

---
+++ build/precache-manifest.025c6caa9401b76299e6197364e87b4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a9426999ce422e2725c3737ed55d75",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fd2574f1652b285f5c2b",
+    "url": "./static/js/2.f8e35e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f8e35e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "653f6046a349c4e2a407",
+    "url": "./static/js/main.7046a1a6.chunk.js"
+  },
+  {
+    "revision": "705f434324a29d64b932",
+    "url": "./static/js/runtime-main.075efe5b.js"
+  }
+]);
